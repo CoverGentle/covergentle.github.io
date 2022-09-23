303fb5633c2ecab4aa592567b282c13ee4b222f1
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-23 10:56:56 +0800

    tj

---
+++ assets/setting.ffc0e09a.js
@@ -0,0 +1,1 @@
+import{d as e,g as t,o,e as n}from"./index.bd28e223.js";const s=n("h1",null,"\u8BBE\u7F6E",-1),a=[s],u=e({__name:"setting",setup(_){return(c,r)=>(o(),t("div",null,a))}});export{u as default};
