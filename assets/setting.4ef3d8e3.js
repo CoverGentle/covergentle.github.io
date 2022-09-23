abe6629d06d735937d82fbc9e3e64cba2e5e21a5
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-23 10:36:59 +0800

    换代

--- assets/setting.4ef3d8e3.js
+++
@@ -1,1 +0,0 @@
-import{d as e,g as t,o,e as n}from"./index.7e30240a.js";const s=n("h1",null,"\u8BBE\u7F6E",-1),a=[s],u=e({__name:"setting",setup(_){return(c,r)=>(o(),t("div",null,a))}});export{u as default};
