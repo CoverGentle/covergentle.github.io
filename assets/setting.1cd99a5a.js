ecde5a80ccd3fce7e0b97860b287996e5794cab3
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-23 13:42:31 +0800

    fb

---
+++ assets/setting.1cd99a5a.js
@@ -0,0 +1,1 @@
+import{d as e,g as t,o,e as n}from"./index.abd753ed.js";const s=n("h1",null,"\u8BBE\u7F6E",-1),a=[s],u=e({__name:"setting",setup(_){return(c,r)=>(o(),t("div",null,a))}});export{u as default};
